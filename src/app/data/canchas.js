c4b4c92ce76d66871f10f3e7956587f40f157437
Camila <camilachliguay2@gmail.com>
2026-09-23 22:01:54 -0300

    Portada con listado de canchas usando map

---
+++ src/app/data/canchas.js
@@ -0,0 +1,23 @@
+export const canchas = [
+  {
+    id: 1,
+    numeroCancha: 1,
+    superficie: "cemento",
+    precioHora: 2000,
+    activa: false,
+  },
+  {
+    id: 2,
+    numeroCancha: 2,
+    superficie: "cemento",
+    precioHora: 2000,
+    activa: true,
+  },
+  {
+    id: 3,
+    numeroCancha: 3,
+    superficie: "cemento",
+    precioHora: 2000,
+    activa: false,
+  },
+];
